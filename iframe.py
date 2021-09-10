04bfb4ce23c52813f7a1fcecdb26778edf207ffb
schwas3 <80727263+schwas3@users.noreply.github.com>
2021-09-10 16:17:06 -0400

    9.10.21 (Good code checkpoint)
    Added file/directory base work
    
    added image interpretation code includes:
     - using PIL to pull images and interpret and manipulate them such as to create the "background" image
     - using cv2 to write temp files which are the concatenation of the original and background adjusted images
     - using cv2 to create .avi file type movie including the side by side comparison
    
    added code that results in accessing external repos (on the users machine, can be any location etc. requires double checking that the folder can be manipulated in this way or if it should not be accessed by the code so as to not be intrusive on users machines).
    
    very experimental build still but a good code checkpoint

--- iframe.py
+++ iframe.py
@@ -5,5 +5,81 @@ import time as t1
 import matplotlib.pyplot as plt
 import os
 import yaml
+import cv2
 
 # WIP
+this_file_path = os.path.realpath(__file__) # gets the path to this file including the file
+this_repo_path, this_file_name = os.path.split(this_file_path) # gets the path to the repository containing this file and the file name
+github_path, this_repo_name = os.path.split(this_repo_path) # gets the users github folder location and the repo name
+## ----- ENTER THE DATA FILE LOCATION INFORMATION HERE ----- ##
+data_repo_name = "Snowball3"
+data_repo_path = github_path + os.path.sep + data_repo_name
+data_folder_name = 'SNOWBALL CROPPED IMAGES' + os.path.sep + 'control 08 - 8 bit'
+data_folder_path = data_repo_path + os.path.sep + data_folder_name
+## ------ ##
+from PIL import Image
+import glob
+img_array = []
+for files in glob.glob(data_folder_path + os.path.sep + '*origandcorr.jpg'):
+    try: 
+        os.remove(files)
+    except: pass
+
+filename = glob.glob(data_folder_path + os.path.sep + '3.603141535*')
+for i in filename:
+    img = Image.open(i)
+    imgarray = np.array(img)
+    img_array.append(imgarray)
+bkgd = img_array[0]/50
+for i in range(49):
+    bkgd = bkgd + img_array[i+1]/50
+imgs = []
+for i in range(len(filename)):
+    # print(i)
+    imG=img_array[i]
+    corr_img=imG-bkgd
+    corr_img = np.where(corr_img<0, 0,corr_img)
+    img=np.concatenate((imG,corr_img),axis=1)
+    img=np.rint(img)
+    # print(img)
+    cv2.imwrite(filename[i]+'origandcorr'+".jpg",img)
+    iMG=cv2.imread(filename[i]+'origandcorr'+".jpg")
+    height, width, layers = iMG.shape
+    size = (width,height)
+    imgs.append(iMG)
+    # imgs.append(img)
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+for i in range(len(imgs)):
+    out.write(imgs[i])
+out.release()
+from os import startfile
+startfile("C:\\Users\\Scott\\Documents\\GitHub\\Snowball2\\project.avi")
+for files in glob.glob(data_folder_path + os.path.sep + '*origandcorr.jpg'):
+    try: 
+        os.remove(files)
+    except: pass
+
+
+# for i in filename:
+    # image_tif = Image.open(filename)
+    # imarray = np.array(image_tif)
+    # imarrayx=imarray
+
+    # I = Image.fromarray(imarray)
+# I.show()
+# img_array = []
+# for filename in glob.glob(data_folder_path + os.path.sep + '3.603141535*'):
+    # img = cv2.imread(filename)
+#     height, width, layers = img.shape
+#     size = (width,height)
+#     img_array.append(img)
+
+
+# out = cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 40, size)
+ 
+# for i in range(len(img_array)):
+#     out.write(img_array[i])
+# out.release()
+# from os import startfile
+# startfile("C:\\Users\\Scott\\Documents\\GitHub\\Snowball2\\project.mp4")
