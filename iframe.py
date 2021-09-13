053215f204570b13748e88ff703767a38592db6f
schwas3 <80727263+schwas3@users.noreply.github.com>
2021-09-13 16:23:24 -0400

    End of day push - comment related changes mostly
    Added some comments, removed artifact code

--- iframe.py
+++ iframe.py
@@ -17,13 +17,11 @@ data_repo_path = github_path + os.path.sep + data_repo_name
 data_folder_name = 'SNOWBALL CROPPED IMAGES' + os.path.sep + 'control 08 - 8 bit'
 data_folder_path = data_repo_path + os.path.sep + data_folder_name
 ## ------ ##
+
+
 from PIL import Image
 import glob
 img_array = []
-# for files in glob.glob(data_folder_path + os.path.sep + '*origandcorr.jpg'):
-#     try: 
-#         os.remove(files)
-#     except: pass
 run = '3.603154717'
 filename = glob.glob(data_folder_path + os.path.sep + run+'*')
 for i in filename:
@@ -35,8 +33,11 @@ bkgd = img_array[1]/50
 for i in range(49):
     bkgd = bkgd + img_array[i+2]/50
 imgs = []
+
+## -------------------------- ##
+## some background calculations and movie preparation as well as side by side with original and background corrected videos ##
+## -------------------------- ##
 for i in range(len(filename)):
-    # print(i)
     imG=img_array[i]
     corr_img=imG-bkgd
     corr_img = np.where(corr_img<0, 0,corr_img)
@@ -45,7 +46,6 @@ for i in range(len(filename)):
     height, width = iMG.shape
     size = (width,height)
     imgs.append(iMG)
-    
 out = cv2.VideoWriter(run+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size, isColor=0)
 
 for i in range(len(imgs)):
@@ -53,31 +53,7 @@ for i in range(len(imgs)):
 out.release()
 from os import startfile
 startfile(this_repo_path+os.path.sep+run+".avi")
-# for files in glob.glob(data_folder_path + os.path.sep + '*origandcorr.jpg'):
-#     try: 
-#         os.remove(files)
-#     except: pass
 
-
-# for i in filename:
-    # image_tif = Image.open(filename)
-    # imarray = np.array(image_tif)
-    # imarrayx=imarray
-
-    # I = Image.fromarray(imarray)
-# I.show()
-# img_array = []
-# for filename in glob.glob(data_folder_path + os.path.sep + '3.603141535*'):
-    # img = cv2.imread(filename)
-#     height, width, layers = img.shape
-#     size = (width,height)
-#     img_array.append(img)
-
-
-# out = cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 40, size)
- 
-# for i in range(len(img_array)):
-#     out.write(img_array[i])
-# out.release()
-# from os import startfile
-# startfile("C:\\Users\\Scott\\Documents\\GitHub\\Snowball2\\project.mp4")
+## -------------------------- ##
+## end of section ##
+## -------------------------- ##
