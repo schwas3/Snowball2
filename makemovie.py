30357da26be785a65de9eeadd9d244a59b6af143
schwas3 <80727263+schwas3@users.noreply.github.com>
2021-09-22 19:58:08 -0400

    9.22.21 (PM8)

---
+++ makemovie.py
@@ -0,0 +1,29 @@
+ #!~/.anaconda3/bin/python
+import math
+import numpy as np
+import time as t1
+import matplotlib.pyplot as plt
+import os
+import yaml
+import cv2
+from PIL import Image
+import glob
+from os import read, startfile, write
+def writeAviVideo(videoName, frameRate, images, openVideo: bool): # creates a video with videoname, frame_rate, using "images", and opens the video based on a boolean
+    # images = np.array(images).astype(np.uint8)
+    height, width, layers = images[0].shape
+    size = (width,height)
+    out = cv2.VideoWriter(videoName+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size) # isColor = 0 can be replaced by changing line (this line + 2 (or 3) to out.write(cv2.merge([imgs[i],imgs[i],imgs[i]]))
+    for i in range(len(images)):
+        out.write(images[i])
+    out.release()
+    if openVideo:
+        startfile(this_repo_path+os.path.sep+videoName+".avi")
+Images = [] # initializes the array used to store images to make a movie
+this_file_path = os.path.realpath(__file__) # gets the path to this file including the file
+this_repo_path, this_file_name = os.path.split(this_file_path) # gets the path to the repository containing this file and the file name
+github_path, this_repo_name = os.path.split(this_repo_path) # gets the users github folder location and the repo name
+filenames = glob.glob(this_repo_path + os.path.sep + '*'+os.path.sep+'*.png')
+for filename in filenames:
+    Images.append(cv2.imread(filename))
+writeAviVideo(videoName = 'Bar Graphs and Histograms Video',frameRate = 1/30,images = Images,openVideo = True)
