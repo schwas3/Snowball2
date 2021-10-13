2d934bb42e5e846cf7a3bcc61807a43da550b5a6
schwas3 <80727263+schwas3@users.noreply.github.com>
2021-10-13 14:04:06 -0400

    10 13 21 2

--- muonVeto/muonVeto3.py
+++ muonVeto/muonVeto3.py
@@ -17,7 +17,7 @@ import glob
 muonVetoPath = 'C:\\Users\\Scott\\Downloads\\ForScott'
 muonVetoIndicesPath = 'C:\\Users\\Scott\\Downloads\\ForScott2'
 resultsPaths = 'C:\\Users\\Scott\\Documents\\GitHub\\Snowball6\\Bar and Hist Figs'
-thisPaths = 'C:\\Users\\Scott\\Documents\\GitHub\\Snowball2'
+thisPaths = 'C:\\Users\\Scott\\Documents\\GitHub\\Snowball2\\muonVeto'
 imagesPaths = 'C:\\Users\\Scott\\Documents\\GitHub\\Snowball9\\SNOWBALL CROPPED IMAGES'
 name = ['control', 'AmBe','AmBe Pb', 'Cs137', 'U','UBe','CfPb']
 Colors = ['b-','r--','r-','g-','c-','y-','m-','y-']
@@ -38,9 +38,10 @@ bounds = 2
 binWidth = 1 / binsPerSecond
 coincedenceRate = binWidth * backgroundFrequency
 fig = plt.figure(figsize=(10,5.63))
+Counts = []
 for i in range(len(name)):
     fileName = name[i]
-    thisFile = open(fileName+' Ag - Muon Delta T (best).txt','r')
+    thisFile = open(thisPaths+os.path.sep+fileName+' Ag - Muon Delta T (best).txt','r')
     thisData = thisFile.read()
     thisFile.close()
     data = [float(i) for i in thisData.split('\n')]
@@ -49,15 +50,18 @@ for i in range(len(name)):
     # data = data[data<.5]
     # data = data[data>-.5]
     counts,bins = np.histogram(data,int(2*bounds*binsPerSecond),range=(-bounds,bounds))
-    # plt.plot(bins[:-1]/2+bins[1:]/2,counts/len(data),Colors[i],label=fileName+' (N='+str(len(data))+')')
-    plt.errorbar(bins[:-1]/2+bins[1:]/2,counts/len(data),np.sqrt(counts)/len(data),binWidth/2,Colors[i],label=fileName+' (N='+str(len(data))+')',linewidth=.5,elinewidth=1)
-    plt.hlines([coincedenceRate,0],-bounds,bounds,colors=['black','black'],linestyles=['dashed','solid'],zorder=0)
+    if i == 0:
+        Counts = counts
+# plt.plot(bins[:-1]/2+bins[1:]/2,counts/len(data),Colors[i],label=fileName+' (N='+str(len(data))+')')
+    plt.errorbar(bins[:-1]/2+bins[1:]/2,counts/len(bigData[i])-Counts/len(bigData[0]),np.sqrt(counts)/len(bigData[i])+np.sqrt(Counts)/len(bigData[0]),binWidth/2,Colors[i],linewidth=0.25,elinewidth=0.5,label=fileName+' (N='+str(len(data))+')')
+    # plt.errorbar(bins[:-1]/2+bins[1:]/2,counts/len(data),np.sqrt(counts)/len(data),binWidth/2,Colors[i],label=fileName+' (N='+str(len(data))+')',linewidth=.5,elinewidth=1)
+    plt.hlines([0,0],-bounds,bounds,colors=['black','black'],linestyles=['dashed','dashed'],zorder=0)
 plt.xlabel('Delta Time (s)')
 plt.ylabel('Probability/bin')
 # plt.plot(bins[:-1]/2+bins[1:]/2,counts/np.sum(counts)/int(2*bounds*binsPerSecond),Colors.pop(0),label=fileName+' (N='+str(len(data))+')')
     # plt.hist(bins[:-1],bins,weights=counts/np.sum(counts),label=fileName+' (N='+str(len(data))+')')
 plt.legend()
-plt.ylim(-.05,.6)
+plt.ylim(-.3,.3)
 plt.suptitle('ALL Ag - MuonVeto Delta t\'s')
 fig.tight_layout()
 plt.savefig('ALL Ag - Muon Hist.jpg')
@@ -66,10 +70,10 @@ for i in range(len(bigData)):
     fileName = name[i]
     subPlot = plt.subplot(int(np.ceil(np.sqrt(len(bigData)))),int(np.ceil(np.sqrt(len(bigData)))),i+1,title=fileName+' (N='+str(len(bigData[i]))+')')
     counts,bins = np.histogram(bigData[i],int(2*bounds*binsPerSecond),range=(-bounds,bounds))
-    plt.plot(bins[:-1]/2+bins[1:]/2,counts/len(bigData[i]),Colors[i],linewidth=0.25)
-    plt.errorbar(bins[:-1]/2+bins[1:]/2,counts/len(bigData[i]),np.sqrt(counts)/len(bigData[i]),binWidth/2,Colors[i],linewidth=0.25,elinewidth=0.5)
-    plt.hlines([coincedenceRate,0],-bounds,bounds,colors=['black','black'],linestyles=['dashed','solid'],zorder=0)
-    plt.ylim(-.05,.6)
+    plt.plot(bins[:-1]/2+bins[1:]/2,counts/len(bigData[i])-Counts/len(bigData[0]),Colors[i],linewidth=0.25)
+    plt.errorbar(bins[:-1]/2+bins[1:]/2,counts/len(bigData[i])-Counts/len(bigData[0]),np.sqrt(counts)/len(bigData[i])+np.sqrt(Counts)/len(bigData[0]),binWidth/2,Colors[i],linewidth=0.25,elinewidth=0.5)
+    plt.hlines([0,0],-bounds,bounds,colors=['black','black'],linestyles=['dashed','dashed'],zorder=0)
+    plt.ylim(-.3,.3)
     plt.xlabel('Delta Time (s)')
     plt.ylabel('Probability/bin')
 plt.suptitle('ALL separate - MuonVeto Delta t\'s')
